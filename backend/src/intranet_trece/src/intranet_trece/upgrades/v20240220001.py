c136a142263b83f96e86dfb848dba92c680ca6c4
Érico Andrei <ericof@gmail.com>
2024-02-20 11:30:40 -0300

    Exercício 12: Corrige formatação do código

--- backend/src/intranet_trece/src/intranet_trece/upgrades/v20240220001.py
+++ backend/src/intranet_trece/src/intranet_trece/upgrades/v20240220001.py
@@ -5,5 +5,10 @@ from plone import api
 
 def cria_estrutura(context):
     portal = api.portal.get()
-    populate_portal(portal, ["TRE-CE",])
+    populate_portal(
+        portal,
+        [
+            "TRE-CE",
+        ],
+    )
     logger.info("Criadas conteúdos para organizar Áreas e Pessoas.")
