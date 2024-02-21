dd68f212611fd8f7f6a6c9276d689f5f29959ea6
Érico Andrei <ericof@gmail.com>
2024-02-21 11:53:45 -0300

    Exercício 23: Adiciona permissão e validadores de ramail e email para o tipo Area

--- backend/src/intranet_trece/src/intranet_trece/content/area.py
+++ backend/src/intranet_trece/src/intranet_trece/content/area.py
@@ -1,19 +1,48 @@
+from intranet_trece import _
 from plone.dexterity.content import Container
+from plone.schema.email import Email
+from plone.supermodel import model
 from plone.supermodel.model import Schema
 from zope import schema
 from zope.interface import implementer
+
+import re
+
+
+def is_valid_email(value: str) -> bool:
+    """Validar se o email é @tre-ce.jus.br."""
+    return value.endswith("@tre-ce.jus.br") if value else True
+
+
+def is_valid_extension(value: str) -> bool:
+    """Validar se o o ramal tem 4 dígitos numéricos."""
+    return re.match(r"^\d{4}$", value) if value else True
 
 
 class IArea(Schema):
     """Definição de uma Área no TRE-CE."""
 
-    # Informações básicas
-    title = schema.TextLine(title="Nome da Área", required=True)
-    description = schema.Text(title="Descrição", required=False)
+    title = schema.TextLine(title=_("Nome da Área"), required=True)
+    description = schema.Text(title=_("Descrição"), required=False)
 
-    # Contato
-    email = schema.TextLine(title="E-mail", required=True)
-    ramal = schema.TextLine(title="Ramal", required=True)
+    model.fieldset(
+        "contato",
+        _("Contato"),
+        fields=[
+            "email",
+            "ramal",
+        ],
+    )
+    email = Email(
+        title=_("Email"),
+        required=True,
+        constraint=is_valid_email,
+    )
+    ramal = schema.TextLine(
+        title=("Ramal"),
+        required=True,
+        constraint=is_valid_extension,
+    )
 
 
 @implementer(IArea)
