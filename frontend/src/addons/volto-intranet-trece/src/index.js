26cd777ede40313d2e098865ec50bd5df66db03d
Érico Andrei <ericof@gmail.com>
2024-02-22 12:43:45 -0300

    Exercício 46: Bloco de clima

--- frontend/src/addons/volto-intranet-trece/src/index.js
+++ frontend/src/addons/volto-intranet-trece/src/index.js
@@ -1,6 +1,13 @@
+// Blocos
+import ClimaEdit from './components/Blocks/Clima/Edit';
+import ClimaView from './components/Blocks/Clima/View';
+
 // Visoes
 import AreaView from './components/View/AreaView';
 import PessoaView from './components/View/PessoaView';
+
+// Icones
+import climaSVG from '@plone/volto/icons/cloud.svg';
 
 const applyConfig = (config) => {
   config.settings = {
@@ -9,6 +16,33 @@ const applyConfig = (config) => {
     supportedLanguages: ['pt-br'],
     defaultLanguage: 'pt-br',
   };
+  // Blocos
+  config.blocks.blocksConfig.climaBlock = {
+    id: 'climaBlock',
+    title: 'Clima',
+    group: 'common',
+    icon: climaSVG,
+    view: ClimaView,
+    edit: ClimaEdit,
+    restricted: false,
+    mostUsed: true,
+    sidebarTab: true,
+    blockHasOwnFocusManagement: false,
+  };
+
+  // Adiciona blocos ao Grid
+  config.blocks.blocksConfig.gridBlock = {
+    ...config.blocks.blocksConfig.gridBlock,
+    blocksConfig: {
+      ...config.blocks.blocksConfig,
+    },
+    allowedBlocks: [
+      ...config.blocks.blocksConfig.gridBlock.allowedBlocks,
+      'climaBlock',
+    ],
+  };
+
+  // Visoes
   config.views.contentTypesViews = {
     ...config.views.contentTypesViews,
     Area: AreaView,
